3c0656ab8d1386f9e7b25b14d42a9d55fa4c776f
vmfernandezg <54022588+vmfernandezg@users.noreply.github.com>
2023-05-16 04:12:02 +0200

    Update main.tf

--- terraform/modules/webapp/main.tf
+++ terraform/modules/webapp/main.tf
@@ -9,12 +9,10 @@ resource "azurerm_app_service_plan" "mi_app_service_plan" {
   #maximum_elastic_worker_count  = 3
   zone_redundant      = true              # IMPORTANTE HA
   #per_site_scaling    = true              # IMPORTANTE PERF
-  
   tags = {
       Module = "Linux App Service Plan"
       Version = "1.0"
   }
-  
   sku {
     tier      = var.tier
     size      = var.size
@@ -28,16 +26,13 @@ resource "azurerm_app_service" "mi_app_service" {
   resource_group_name = var.resource_group_name
   app_service_plan_id = azurerm_app_service_plan.mi_app_service_plan.id
   https_only            = true
-
   site_config {
     always_on         = true
     #number_of_workers = "3"               # IMPORTANTE PERF
   }
-  
   app_settings = {
     "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
   }
-
   tags = {
       Module = "Linux App Service"
       Version = "1.0"
