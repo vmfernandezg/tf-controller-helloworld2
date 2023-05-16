0048174970b6f766ef9fba46ef2bfd469b90d7ad
vmfernandezg <54022588+vmfernandezg@users.noreply.github.com>
2023-05-16 03:54:09 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,12 +19,3 @@ module "webapp" {
   size                = "P1v3"
   capacity            = "3"
 }
-
-#  Deploy code from a public GitHub repo
-resource "azurerm_app_service_source_control" "sourcecontrol" {
-  app_id             = azurerm_app_service_plan.azurerm_app_service.id
-  repo_url           = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
-  branch             = "master"
-  use_manual_integration = true
-  use_mercurial      = false
-}
